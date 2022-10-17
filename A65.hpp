a3775a0b736a7409d6a82e2cc69350bf462498f4
kojosh <111550404+kojosh@users.noreply.github.com>
2022-10-16 23:10:09 -0700

    Update A65.hpp

--- A65.hpp
+++ A65.hpp
@@ -25,15 +25,3 @@ void swaptwo(int &A, int &B)
     A = B;
     B = temp;
 }
-
-int main()
-{
-    int num1, num2, num3;
-    cout << "Enter three integers: ";
-    cin >> num1 >> num2 >> num3;
-
-    swap(num1, num2, num3);
-
-    cout << num1 << " " << num2 << " " << num3 << endl;
-
-}
